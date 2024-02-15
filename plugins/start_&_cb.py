7db3933f7c1e7d5b27cda45af012ee6a6f1cd7c8
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-02-15 17:43:03 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -76,7 +76,7 @@ async def cb_handler(client, query: CallbackQuery):
         )
     
     elif data == "file_names":
-        format_template = await db.get_format_template(user_id)
+        format_template = await madflixbotz.get_format_template(user_id)
         await query.message.edit_text(
             text=Txt.FILE_NAME_TXT.format(format_template=format_template),
             disable_web_page_preview=True,
