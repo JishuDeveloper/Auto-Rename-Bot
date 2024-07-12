9df359eb901ba9c5d4b08f9180bc9017c89fe570
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-07-13 02:53:41 +0530

    Peer ID Invalid Problem Fixed ✅

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ from pyrogram.raw.all import layer
 from config import Config
 from aiohttp import web
 from route import web_server
+import pyrogram.utils
+
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+
 
 class Bot(Client):
 
